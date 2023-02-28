9202a444913d167ad306fde7d8bf32c4cc7f2c75
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-02-28 20:15:39 +0530

    Added a Program to Create a Linked List

--- Program.cs
+++ Program.cs
@@ -5,7 +5,14 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to LinkedList Program");
+            LinkedList linked = new LinkedList();
+            linked.Add(56);
+            linked.Add(30);
+            linked.Add(70);
+
+            linked.Display();
         }
     }
-}
+    }
+
 
