5272ed38afc7d362487e65412676bbba3cc027b0
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-02-28 21:46:49 +0530

    Insert Between Linked list

--- LinkedList.cs
+++ LinkedList.cs
@@ -12,26 +12,48 @@ namespace LinkedList
 
         public Node head;
 
-        internal void Append(int data)
+        public void AddAtPosition(int position, int data)
         {
-            Node node = new Node(data);
+            Node newNode = new Node(data);
 
-            if (this.head == null)
+            if (position < 1)
             {
-                this.head = node;
+                Console.WriteLine("Invalid position");
+            }
+            //If we want node to be inserted at first position
+            if (position == 1)
+            {
+                newNode.next = head;
+                head = newNode;
             }
             else
             {
-                Node temp = head;
-
-                while (temp.next != null)
+                //Declaring count to count starting from 2 the position
+                int currentPosition = 1;
+                //Declaring temporary head tempHead to parse throu the list
+                Node tempHead = head;
+                //while count does not get to the entered position where we want to add our node
+                while (currentPosition++ != position)
                 {
-                    temp = temp.next;
-                }
+                    //If our count variable is at entered position
+                    if (currentPosition == position)
+                    {
+                        //Assigning newNode's next to tempHead's next, the position at which we want to insert node
+                        newNode.next = tempHead.next;
 
-                temp.next = node;
+                        //Assigning previous node's (here tempHead) next to newNode
+                        tempHead.next = newNode;
+                        Console.WriteLine(newNode.data + " is added at position " + position);
+                        break;
+                    }
+                    //Moving the position of tempHead to the next node
+                    tempHead = tempHead.next;
+                }
+                if (currentPosition != position)
+                {
+                    Console.WriteLine("Position is out of range");
+                }
             }
-            Console.WriteLine(node.data + " appended to the Linked List");
         }
 
         public void Add(int data)
@@ -68,7 +90,7 @@ namespace LinkedList
                 Console.Write(temp.data + " ");
                 temp = temp.next;
             }
-
+            Console.WriteLine();
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,13 +6,17 @@
         {
             Console.WriteLine("Welcome to LinkedList Program");
 
-            Console.WriteLine("\nCreate Linked List by Appending 30 and 70 to 56\n");
+
+            Console.WriteLine("\nInserting 30 between 56 and 70\n");
 
             LinkedList linked = new LinkedList();
-            linked.Append(56);
-            linked.Append(30);
-            linked.Append(70);
+            linked.Add(56);
+            linked.Add(70);
+            linked.Display();
 
+            Console.WriteLine("After Inserting:");
+
+            linked.AddAtPosition(2, 30);
             linked.Display();
         }
     }
